58dc0180af532a1dce5944da375253f30f99d7f6
Diiegoorodrigo <diegorodrigosilva76@gmail.com>
2022-03-25 09:07:34 -0300

    new

---
+++ Projetos/Starbucks/script.js
@@ -0,0 +1,8 @@
+function slider (anything) {
+    document.querySelector('.thumb').src = anything;
+
+}
+function changeCollor (color) {
+    const extra = document.querySelector('.extras');
+    extra.style.backgroundColor = color;;
+}
