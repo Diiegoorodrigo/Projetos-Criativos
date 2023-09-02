93118040c84694bd6228bce0f8590551326aefa6
Diiegoorodrigo <diegorodrigosilva76@gmail.com>
2023-09-02 18:26:43 -0300

    ASDF

--- Projetos/FalaComigo/script.js
+++ Projetos/FalaComigo/script.js
@@ -15,7 +15,7 @@ btn.addEventListener('click', function(){
 })
 
 const txt = document.querySelector('.txt');
-const text ='Bem vinda! Sei que algumas coisas não podem ser desfeitas, mas podemos procurar acertar. Sei que errei com você, fiz algo que prometi a você que não faria e isso acabou comigo pois vi o qual idiota eu fui mas quero lhe pedir desculpas do fundo do meu coração, sei que apenas palavras não é o bastante por isso irei me esforçar para mostrar que realmente me importo contigo e que tenho um grande carinho por você, Lorena quero lhe pedir perdão e que me permita permanecer próximo de você! ';
+const text ='Bem vinda! Sei que algumas coisas não podem ser desfeitas, mas podemos procurar acertar. Sei que errei com você, fiz algo que prometi a você que não faria e isso acabou comigo pois vi o quão idiota eu fui mas quero lhe pedir desculpas do fundo do meu coração, sei que apenas palavras não é o bastante por isso irei me esforçar para mostrar que realmente me importo contigo e que tenho um grande carinho por você, Lorena quero lhe pedir perdão e que me permita permanecer próximo de você! ';
 const interval = 100;
 
 function showText(txt, text, interval) {
